cfd8ecbd737d459af318943853993a2fe72b4e2d
LisisFd <lisis.fd@gmail.com>
2023-11-17 20:59:56 +0200

    refactor

--- app/app_main/lib/view/widgets/widgets.dart
+++ app/app_main/lib/view/widgets/widgets.dart
@@ -1,4 +1,4 @@
 //GENERATED BARREL FILE
 export 'icon_text_form_field.dart';
-export 'locale_listener_widget.dart';port 'scaffold_my_app.dart';
-
+export 'locale_listener_widget.dart';
+export 'scaffold_my_app.dart';
